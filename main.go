ae548636eb2e396f7187474904b07c9afcae387a
Lucas Rosa <lucas.furlani.rosa@gmail.com>
2019-10-31 20:59:46 -0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/gin-gonic/gin"
+)
+
+// TODO fill these in!
+const (
+	S3_REGION = "us-east-1"
+	S3_BUCKET = "go-sc-test-s3"
+)
+
+func main() {
+
+	router := gin.Default()
+	// Set a lower memory limit for multipart forms (default is 32 MiB)
+	// router.MaxMultipartMemory = 8 << 20  // 8 MiB
+	router.POST("/upload", func(c *gin.Context) {
+		// single file
+		file, _ := c.FormFile("file")
+		log.Println(file.Filename)
+
+		// Upload the file to specific dst.
+		err := c.SaveUploadedFile(file, "/Users/lucas/Documents/potato/1.png")
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		c.String(http.StatusOK, fmt.Sprintf("'%s' uploaded!", file.Filename))
+
+		// // Create a single AWS session (we can re use this if we're uploading many files)
+		s, err := session.NewSession(&aws.Config{Region: aws.String(S3_REGION)})
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Upload
+		err = AddFileToS3(s, "/Users/lucas/Documents/potato/1.png")
+		if err != nil {
+			log.Fatal(err)
+		}
+	})
+	router.Run(":8080")
+}
+
+// AddFileToS3 will upload a single file to S3, it will require a pre-built aws session
+// and will set file info like content type and encryption on the uploaded file.
+func AddFileToS3(s *session.Session, fileDir string) error {
+
+	// Open the file for use
+	file, err := os.Open(fileDir)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	// Get file size and read the file content into a buffer
+	fileInfo, _ := file.Stat()
+	var size int64 = fileInfo.Size()
+	buffer := make([]byte, size)
+	file.Read(buffer)
+
+	// Config settings: this is where you choose the bucket, filename, content-type etc.
+	// of the file you're uploading.
+	_, err = s3.New(s).PutObject(&s3.PutObjectInput{
+		Bucket:               aws.String(S3_BUCKET),
+		Key:                  aws.String(fileDir),
+		ACL:                  aws.String("private"),
+		Body:                 bytes.NewReader(buffer),
+		ContentLength:        aws.Int64(size),
+		ContentType:          aws.String(http.DetectContentType(buffer)),
+		ContentDisposition:   aws.String("attachment"),
+		ServerSideEncryption: aws.String("AES256"),
+	})
+	return err
+}
